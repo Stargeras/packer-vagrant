ac8890b76172740ae9ec79b275130addee234214
Stargeras <dillongrosso@live.com>
2022-06-04 16:20:13 -0400

    updates

--- el8/common/firefox-edits.sh
+++ el8/common/firefox-edits.sh
@@ -1,9 +1,10 @@
-cat >> /etc/firefox/pref/firefox.js << FOE
+cat > /etc/firefox/pref/firefox.js << FOE
 pref("browser.tabs.drawInTitlebar", true);
 pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"home-button\",\"urlbar-container\",\"downloads-button\",\"library-button\",\"sidebar-button\",\"fxa-toolbar-menu-button\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"personal-bookmarks\"]},\"seen\":[\"developer-button\"],\"dirtyAreaCache\":[],\"currentVersion\":16,\"newElementCount\":2}");
 pref("app.normandy.first_run", false);
 pref("browser.aboutwelcome.enabled", false);
-pref("browser.startup.firstrunSkipsHomepage". false);
+pref("browser.startup.firstrunSkipsHomepage", false);
+pref("browser.toolbars.bookmarks.visibility", "newtab"); //always reverts anyways
 pref("startup.homepage_welcome_url","");
 pref("startup.homepage_override_url","");
 pref("datareporting.policy.firstRunURL",""); //stop firefox privacy page from showing up
